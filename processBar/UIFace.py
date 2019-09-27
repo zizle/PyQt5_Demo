88a5168aff14b1a102f612386b51bd403001879e
zizle <zizle_lin@163.com>
2019-09-27 14:43:24 +0800

    各种进度条

--- processBar/UIFace.py
+++ processBar/UIFace.py
@@ -2,42 +2,25 @@
 # Author: zizle
 """
 对于样式的改变：
-主要看主窗口Surface类的初始化内的setStyleSheet
+常规样式主要看主窗口Surface类的初始化内的setStyleSheet
 """
-
-import sys
+import copy
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QTimer
-
-
-class CustomerProcessBar(QtWidgets.QProgressBar):
-    def __init__(self):
-        super(CustomerProcessBar, self).__init__()
-        self.setValue(0)
-        self.setMaximum(100)
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.timer_out)
-
-    def timer_out(self):
-        if self.value() >= 100:
-            self.timer.stop()
-            return
-        self.setValue(self.value() + 1)
-
-    def timer_start(self):
-        if self.timer.isActive():
-            return
-        self.timer.start(200)
-
-    def timer_stop(self):
-        if self.timer.isActive():
-            self.timer.stop()
+from bag import special
 
 
 class Surface(QtWidgets.QWidget):
     def __init__(self):
         super(Surface, self).__init__()
-        layout = QtWidgets.QVBoxLayout()
+        # 定时器模拟进度
+        self.timer = QTimer()
+        layout = QtWidgets.QHBoxLayout()
+        normal_layout = QtWidgets.QVBoxLayout()
+        special_layout = QtWidgets.QVBoxLayout()
+        normal_group = QtWidgets.QGroupBox('常规样式')
+        special_group = QtWidgets.QGroupBox('特殊样式')
+        """常规样式"""
         # 分组
         group1 = QtWidgets.QGroupBox("常规进度条")
         group2 = QtWidgets.QGroupBox("百分比居中")
@@ -46,9 +29,14 @@ class Surface(QtWidgets.QWidget):
         group5 = QtWidgets.QGroupBox("格子进度条")
         group6 = QtWidgets.QGroupBox("填充方式")
         group7 = QtWidgets.QGroupBox("繁忙状态")
+        group8 = QtWidgets.QGroupBox("圆圈繁忙进度")
+        group9 = QtWidgets.QGroupBox("圆圈百分比")
+        group10 = QtWidgets.QGroupBox("Metro加载中")
+        group11 = QtWidgets.QGroupBox("水波浪")
         # 组1控件布局
         layout1 = QtWidgets.QVBoxLayout()
-        process1 = CustomerProcessBar()
+        process1 = QtWidgets.QProgressBar()
+        process1.timer = None  # 定时器模拟进度
         button1 = QtWidgets.QPushButton('Start')
         button1.clicked.connect(lambda: self.start_stop(button1, process1))
         layout1.addWidget(process1)
@@ -56,8 +44,9 @@ class Surface(QtWidgets.QWidget):
         group1.setLayout(layout1)
         # 组2控件布局
         layout2 = QtWidgets.QVBoxLayout()
-        process2 = CustomerProcessBar()
+        process2 = QtWidgets.QProgressBar()
         process2.setObjectName('process2')
+        process2.timer = None
         button2 = QtWidgets.QPushButton('Start')
         button2.clicked.connect(lambda: self.start_stop(button2, process2))
         layout2.addWidget(process2)
@@ -65,8 +54,9 @@ class Surface(QtWidgets.QWidget):
         group2.setLayout(layout2)
         # 组3控件布局
         layout3 = QtWidgets.QVBoxLayout()
-        process3 = CustomerProcessBar()
+        process3 = QtWidgets.QProgressBar()
         process3.setObjectName('process3')
+        process3.timer = None
         button3 = QtWidgets.QPushButton('Start')
         button3.clicked.connect(lambda: self.start_stop(button3, process3))
         layout3.addWidget(process3)
@@ -74,7 +64,8 @@ class Surface(QtWidgets.QWidget):
         group3.setLayout(layout3)
         # 组4控件布局
         layout4 = QtWidgets.QVBoxLayout()
-        process4 = CustomerProcessBar()
+        process4 = QtWidgets.QProgressBar()
+        process4.timer = None
         process4.setObjectName('process4')
         button4 = QtWidgets.QPushButton('Start')
         button4.clicked.connect(lambda: self.start_stop(button4, process4))
@@ -83,8 +74,9 @@ class Surface(QtWidgets.QWidget):
         group4.setLayout(layout4)
         # 组5控件布局
         layout5 = QtWidgets.QVBoxLayout()
-        process5 = CustomerProcessBar()
+        process5 = QtWidgets.QProgressBar()
         process5.setObjectName('process5')
+        process5.timer = None
         button5 = QtWidgets.QPushButton('Start')
         button5.clicked.connect(lambda: self.start_stop(button5, process5))
         layout5.addWidget(process5)
@@ -92,8 +84,9 @@ class Surface(QtWidgets.QWidget):
         group5.setLayout(layout5)
         # 组6控件布局
         layout6 = QtWidgets.QVBoxLayout()
-        process6 = CustomerProcessBar()
+        process6 = QtWidgets.QProgressBar()
         process6.setObjectName('process6')
+        process6.timer = None
         process6.setTextVisible(False)  # 进度文本不可见
         button6 = QtWidgets.QPushButton('Start')
         button6.clicked.connect(lambda: self.start_stop(button6, process6))
@@ -102,21 +95,83 @@ class Surface(QtWidgets.QWidget):
         group6.setLayout(layout6)
         # 组7控件布局
         layout7 = QtWidgets.QVBoxLayout()
-        process7 = CustomerProcessBar()
+        process7 = QtWidgets.QProgressBar()
         process7.setObjectName('process7')
+        process7.timer = None
         process7.setTextVisible(False)  # 进度文本不可见
         process7.setMinimum(0)
         process7.setMaximum(0)
         layout7.addWidget(process7)
         group7.setLayout(layout7)
-        # 布局添加组
-        layout.addWidget(group1)
-        layout.addWidget(group2)
-        layout.addWidget(group3)
-        layout.addWidget(group4)
-        layout.addWidget(group5)
-        layout.addWidget(group6)
-        layout.addWidget(group7)
+        """特殊样式"""
+        # 组8控件布局,颜色设置查看styleSheet{#process8}
+        layout8 = QtWidgets.QHBoxLayout()
+        process8_1 = special.CircleProgressBar()
+        process8_1.setObjectName('process8')
+        process8_2 = special.CircleProgressBar(clockwise=False)
+        layout8.addWidget(process8_1)
+        layout8.addWidget(process8_2)
+        group8.setLayout(layout8)
+        # 组9控件布局
+        layout9 = QtWidgets.QHBoxLayout()
+        # 顺时针
+        layout9_1 = QtWidgets.QVBoxLayout()
+        process9_1 = special.PercentProgressBar()
+        process9_1.setObjectName('process91')
+        process9_1.timer = None
+        button9_1 = QtWidgets.QPushButton('Start')
+        button9_1.clicked.connect(lambda: self.start_stop(button9_1, process9_1))
+        layout9_1.addWidget(process9_1)
+        layout9_1.addWidget(button9_1)
+        # 逆时针初始化传入参数clockwise=False
+        # (不显示百分号初始参数：showPercent=False)
+        layout9_2 = QtWidgets.QVBoxLayout()
+        process9_2 = special.PercentProgressBar(clockwise=False)
+        process9_2.setObjectName('process92')
+        process9_2.timer = None
+        button9_2 = QtWidgets.QPushButton('Start')
+        button9_2.clicked.connect(lambda: self.start_stop(button9_2, process9_2))
+        layout9_2.addWidget(process9_2)
+        layout9_2.addWidget(button9_2)
+        # 外围阴影小圈 showFreeArea=True
+        # (外围没有小圈，进度条小圆头：showSmallCircle=True)
+        layout9_3 = QtWidgets.QVBoxLayout()
+        process9_3 = special.PercentProgressBar(showFreeArea=True, showSmallCircle=True)
+        process9_3.setObjectName('process93')
+        process9_3.timer = None
+        button9_3 = QtWidgets.QPushButton('Start')
+        button9_3.clicked.connect(lambda: self.start_stop(button9_3, process9_3))
+        layout9_3.addWidget(process9_3)
+        layout9_3.addWidget(button9_3)
+        # 设置颜色，查看styleSheet{#process94}
+        layout9_4 = QtWidgets.QVBoxLayout()
+        process9_4 = special.PercentProgressBar(showFreeArea=True)
+        process9_4.setObjectName('process94')
+        process9_4.timer = None
+        button9_4 = QtWidgets.QPushButton('Start')
+        button9_4.clicked.connect(lambda: self.start_stop(button9_4, process9_4))
+        layout9_4.addWidget(process9_4)
+        layout9_4.addWidget(button9_4)
+
+        layout9.addLayout(layout9_1)
+        layout9.addLayout(layout9_2)
+        layout9.addLayout(layout9_3)
+        layout9.addLayout(layout9_4)
+        group9.setLayout(layout9)
+
+        # 常规布局添加组
+        normal_layout.addWidget(group1)
+        normal_layout.addWidget(group2)
+        normal_layout.addWidget(group3)
+        normal_layout.addWidget(group4)
+        normal_layout.addWidget(group5)
+        normal_layout.addWidget(group6)
+        normal_layout.addWidget(group7)
+        # 特殊布局添加组
+        special_layout.addWidget(group8)
+        special_layout.addWidget(group9)
+        special_layout.addWidget(group10)
+        special_layout.addWidget(group11)
         # 样式
         button1.setMaximumWidth(100)
         button2.setMaximumWidth(100)
@@ -124,8 +179,12 @@ class Surface(QtWidgets.QWidget):
         button4.setMaximumWidth(100)
         button5.setMaximumWidth(100)
         button6.setMaximumWidth(100)
-
-        self.setFixedWidth(600)
+        button9_1.setMaximumWidth(100)
+        button9_2.setMaximumWidth(100)
+        button9_3.setMaximumWidth(100)
+        button9_4.setMaximumWidth(100)
+        normal_group.setFixedWidth(500)
+        special_group.setFixedWidth(500)
         self.setWindowTitle('PyQt5之进度条样式设置')
         self.setStyleSheet("""
         #process2, #process3 {
@@ -163,22 +222,54 @@ class Surface(QtWidgets.QWidget):
             background-color: #575757;
             border-radius: 6px;
         }
+        #process8 {
+            qproperty-color: #575757; /*颜色*/
+        }
+        #process94 {
+            qproperty-textColor: rgb(255, 0, 0);
+            qproperty-borderColor: rgb(0, 255, 0);
+            qproperty-backgroundColor: rgb(0, 0, 255);
+        }
         """)
+        normal_group.setLayout(normal_layout)
+        special_group.setLayout(special_layout)
+        layout.addWidget(normal_group)
+        layout.addWidget(special_group)
         self.setLayout(layout)
 
     def start_stop(self, button, process_bar):
+        if process_bar.timer is None:
+            process_bar.timer = QTimer()
+            process_bar.timer.timeout.connect(lambda: self.time_out(process_bar))
         if button.text() == "Start":
-            process_bar.timer_start()
+            if process_bar.timer.isActive():
+                return
+            process_bar.timer.start(200)
             button.setText('Stop')
         else:
-            process_bar.timer_stop()
+            process_bar.timer.stop()
             button.setText("Start")
         # 如果进度满则重新开始
-        if process_bar.value() >= 100:
+        try:
+            current = process_bar.value()
+        except Exception:
+            current = process_bar.value
+        if current >= 100:
             process_bar.setValue(0)
+
+    def time_out(self, process_bar):
+        try:
+            current = process_bar.value()
+        except Exception:
+            current = process_bar.value
+        if current >= 100:
+            process_bar.timer.stop()
+            return
+        process_bar.setValue(current + 1)
 
 
 if __name__ == '__main__':
+    import sys
     app = QtWidgets.QApplication(sys.argv)
     surface = Surface()
     surface.show()
