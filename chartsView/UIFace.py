41c81470dbf065cc5ca423e39338960cb8e40195
zizle <zizle_lin@163.com>
2019-09-30 15:25:07 +0800

    柱形堆叠

--- chartsView/UIFace.py
+++ chartsView/UIFace.py
@@ -1,7 +1,7 @@
 # _*_ coding:utf-8 _*_
 # __Author__： zizle
 from PyQt5 import QtWidgets
-from chartsView.bag import lineChart, lineStackChart, customerAxis
+from chartsView.bag import lineChart, lineStackChart, customerAxis, barStackChart
 
 
 class Surface(QtWidgets.QWidget):
@@ -10,7 +10,7 @@ class Surface(QtWidgets.QWidget):
         layout = QtWidgets.QGridLayout()
         # 折线图
         line = lineChart.LineChart()
-        # 折线堆叠
+        # 折线堆叠鼠标交互
         line_stack = lineStackChart.LineStackChart()
         # 自定义轴
         customer_xaxis = customerAxis.CustomerAxis()
@@ -18,11 +18,14 @@ class Surface(QtWidgets.QWidget):
         customer_top_xaxis = customerAxis.TopXAxis()
         # 自定义添加右侧Y轴
         customer_right_yaxis = customerAxis.YAxis()
+        # 柱状图堆叠鼠标交互
+        bar_stack = barStackChart.BarStackChart()
         layout.addWidget(line, 0, 0)
-        layout.addWidget(line_stack, 1, 0)
+        layout.addWidget(line_stack, 1, 2)
         layout.addWidget(customer_xaxis, 0, 1)
         layout.addWidget(customer_top_xaxis, 1, 1)
         layout.addWidget(customer_right_yaxis, 0, 2)
+        layout.addWidget(bar_stack, 1, 0)
         self.resize(1360, 768)
         self.setLayout(layout)
 
